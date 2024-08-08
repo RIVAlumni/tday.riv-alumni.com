3e09cc51a0e1a34bcae51209362bd40bcf16ab26
Aaron Teo <taronaeo@gmail.com>
2024-08-08 23:42:52 +0800

    feat(fonts): add geist serif, geist mono, and fontawesome icons

---
+++ src/routes/+layout.ts
@@ -0,0 +1,11 @@
+import '../app.css';
+
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import '@fontsource/geist-sans/400.css';
+import '@fontsource/geist-sans/500.css';
+import '@fontsource/geist-sans/600.css';
+import '@fontsource/geist-sans/700.css';
+import '@fontsource/geist-sans/800.css';
+import '@fontsource/geist-sans/900.css';
+import '@fontsource/geist-mono';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,8 @@ const config: Config = {
         sm: 'calc(var(--radius) - 4px)',
       },
       fontFamily: {
-        sans: [...fontFamily.sans],
+        sans: ['Geist Sans', 'sans-serif'],
+        mono: ['Geist Mono', 'monospace'],
       },
     },
   },
