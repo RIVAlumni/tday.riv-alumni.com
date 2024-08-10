88e5ab74d0d3054b082cc1dfe5addb010d288e27
Aaron Teo <taronaeo@gmail.com>
2024-08-11 01:00:51 +0800

    refactor(global): remove unneccessary user fields

--- src/lib/models/FSUser.ts
+++ src/lib/models/FSUser.ts
@@ -1,11 +1,19 @@
 import type { FSMetadata } from '$lib/models';
+import type { FieldValue } from 'firebase/firestore';
+
+enum FSUserAccessLevel {
+  None = 0,
+  Operator = 1,
+  Mediator = 2,
+}
 
 interface FSUser extends FSMetadata {
   readonly uid: string;
   readonly email: string | null;
-  readonly email_verified: boolean;
   readonly display_name: string | null;
-  readonly photo_url: string | null;
+  readonly access_level: FSUserAccessLevel;
+  readonly access_expires: FieldValue;
 }
 
 export type { FSUser };
+export { FSUserAccessLevel };
