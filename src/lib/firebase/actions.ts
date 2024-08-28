61717fa352ef185c38248791565fd83acddabe21
Aaron Teo <taronaeo@gmail.com>
2024-08-29 02:43:41 +0800

    refactor(global): change date reporting to local

--- src/lib/firebase/actions.ts
+++ src/lib/firebase/actions.ts
@@ -3,6 +3,7 @@ import type { FSUser, FSRegistration } from '$lib/models';
 import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
 
 import { firestore } from '$lib/firebase';
+import { FSUserAccessLevel } from '$lib/models';
 import { colRegistrationsRef } from './firestore';
 
 const currentYear = new Date().getFullYear().toString();
@@ -11,29 +12,32 @@ export async function actionCheckIn(
   currentUser: FSUser,
   currentRecord: FSRegistration,
 ) {
+  const timestamp = new Date();
   const { registration_id, status, comments, arrived_at } = currentRecord;
 
-  if (status === 'CHECKED_IN' || arrived_at !== null)
-    throw new Error(
-      'Already checked in. Please approach the Conflict Resolution Team for assistance.',
-    );
+  if (currentUser.access_level === FSUserAccessLevel.Operator) {
+    if (status === 'CHECKED_IN' || arrived_at !== null)
+      throw new Error(
+        'Already checked in. Please approach the Conflict Resolution Team for assistance.',
+      );
 
-  if (status === 'REJECTED' || arrived_at !== null)
-    throw new Error(
-      'Rejected or already checked in. Please approach the Conflict Resolution Team for assistance.',
-    );
+    if (status === 'REJECTED' || arrived_at !== null)
+      throw new Error(
+        'Rejected or already checked in. Please approach the Conflict Resolution Team for assistance.',
+      );
 
-  if (status === 'REFUSED' || arrived_at !== null) {
-    throw new Error(
-      'Refused entry or already checked in. Please approach the Conflict Resolution Team for assistance.',
-    );
+    if (status === 'REFUSED' || arrived_at !== null) {
+      throw new Error(
+        'Refused entry or already checked in. Please approach the Conflict Resolution Team for assistance.',
+      );
+    }
   }
 
   const newRecord = {
     ...currentRecord,
     status: 'CHECKED_IN',
     comments:
-      `${comments}\n\n(Check-in completed by ${currentUser.display_name?.toUpperCase()} on ${new Date().toUTCString()})`.trim(),
+      `${comments}\n\n(Check-in completed by ${currentUser.display_name?.toUpperCase()} on ${timestamp.toLocaleDateString()} at ${timestamp.toLocaleTimeString()} hours)`.trim(),
     arrived_at: serverTimestamp(),
     updated_at: serverTimestamp(),
   };
@@ -52,13 +56,14 @@ export async function actionRefuseEntry(
   currentUser: FSUser,
   currentRecord: FSRegistration,
 ) {
+  const timestamp = new Date();
   const { registration_id, comments } = currentRecord;
 
   const newRecord = {
     ...currentRecord,
     status: 'REFUSED',
     comments:
-      `${comments}\n\n(Entry refused by ${currentUser.display_name?.toUpperCase()} on ${new Date().toUTCString()})`.trim(),
+      `${comments}\n\n(Entry refused by ${currentUser.display_name?.toUpperCase()} on ${timestamp.toLocaleDateString()} at ${timestamp.toLocaleTimeString()} hours)`.trim(),
     updated_at: serverTimestamp(),
   };
 
@@ -76,13 +81,14 @@ export async function actionSetConflict(
   currentUser: FSUser,
   currentRecord: FSRegistration,
 ) {
+  const timestamp = new Date();
   const { registration_id, comments } = currentRecord;
 
   const newRecord = {
     ...currentRecord,
     status: 'CONFLICT',
     comments:
-      `${comments}\n\n(Handover initiated by ${currentUser.display_name?.toUpperCase()} on ${new Date().toUTCString()})`.trim(),
+      `${comments}\n\n(Handover initiated by ${currentUser.display_name?.toUpperCase()} on ${timestamp.toLocaleDateString()} at ${timestamp.toLocaleTimeString()} hours)`.trim(),
     updated_at: serverTimestamp(),
   };
 
