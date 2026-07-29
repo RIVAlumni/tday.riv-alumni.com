4a686c5ca2158f9cd47cac7e56b458b22dcb96a9
Aaron Teo <aaron.teo1@ibm.com>
2026-07-30 00:04:29 +0800

    chore: pull back the pre-reg timeline
    Signed-off-by: Aaron Teo <aaron.teo1@ibm.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { mdsvex } from 'mdsvex';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
@@ -17,9 +17,6 @@ export default defineConfig({
         experimental: { async: true },
       },
 
-      // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-      // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-      // See https://svelte.dev/docs/kit/adapters for more information about adapters.
       adapter: adapter(),
       preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
       extensions: ['.svelte', '.svx', '.md'],
