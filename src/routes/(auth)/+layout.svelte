46306a1a52722da773f3e1c0a7b42210dd03d7f7
Aaron Teo <aaron.teo1@ibm.com>
2026-07-30 15:59:56 +0800

    feat: redirect authenticated users away from login to /workflow/reception

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { internalAuth } from '$lib/firebase/auth.svelte';
+
+	let { children } = $props();
+
+	onMount(() => {
+		internalAuth.init();
+	});
+
+	// If already logged in, skip the login page
+	$effect(() => {
+		if (!internalAuth.loading && internalAuth.user) {
+			goto('/workflow/reception');
+		}
+	});
+</script>
+
+{#if internalAuth.loading}
+	<div class="flex h-screen items-center justify-center">
+		<p>Loading…</p>
+	</div>
+{:else if !internalAuth.user}
+	{@render children()}
+{/if}
