3367417c014b888cedaed42712c503f44445b694
Aaron Teo <aaron.teo1@ibm.com>
2026-07-30 15:30:56 +0800

    feat: add Firebase app initialization and barrel export

---
+++ src/lib/firebase/index.ts
@@ -0,0 +1,6 @@
+export { VISITOR_CONFIG, INTERNAL_CONFIG, USE_EMULATORS } from './config';
+export type { FirebaseProjectConfig } from './config';
+export { getVisitorApp, getInternalApp, getApp } from './app';
+export { visitorAuth, internalAuth } from './auth.svelte';
+export { getFirestoreForApp, getVisitorFirestore, getInternalFirestore } from './firestore';
+export type { FirebaseAppName, AuthStore } from './types';
